871a5d09a0a165dc0821f77eeaa9f9f8a4470d41
nuno <ngplbc@gmail.com>
2020-03-11 10:51:27 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Express + MySQL boilerplate
+
+## Run the app locally (in development):
+```
+yarn dev
+```
+note: there's a `yarn start` but that is for **production**
+
+## Add your MySQL credentials
+Create a file in the root of the project called `.env` with the following:
+
+```
+MYSQL_HOST=your host
+MYSQL_USERNAME=your username
+MYSQL_PASSWORD=your password
+MYSQL_DB=your db name
+```
---
+++ app.js
@@ -0,0 +1,34 @@
+const createError = require("http-errors");
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+// middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.error(err);
+
+    if (app.get("env") === "development") {
+        return res.json({ error: err, message: err.message });
+    }
+    return res.json({ error: "Sorry, there was a problem... :(" });
+});
+
+module.exports = app;
