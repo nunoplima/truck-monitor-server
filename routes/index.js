2c9b98ac2a5a9cd37cedb7842041ff7aecec6cc6
nuno <ngplbc@gmail.com>
2020-03-14 14:27:36 +0000

    add test route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { getAllTrips } = require("../controllers/trips-controller");
 
+// testing...
+router.get("/", (req, res) => res.status(200).json({ message: "Server is live" }));
+
 // get all active trips and respective postitions from all trucks
 router.get("/all", getAllTrips);
 
