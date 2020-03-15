019b394acf0f6dcafbc4cbf10e746f53218e306a
nuno <ngplbc@gmail.com>
2020-03-15 01:19:18 +0000

    name router

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const indexRouter = require("./routes/index");
+const tripsRouter = require("./routes/tripsRouter");
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("/", indexRouter);
+app.use("/trips", tripsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/tripsRouter.js
+++ routes/tripsRouter.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { getAllTrips } = require("../controllers/trips-controller");
 
 // testing...
-router.get("/", (req, res) => res.status(200).json({ message: "Server is live" }));
+router.get("/test", (req, res) => res.status(200).json({ message: "Server is live" }));
 
 // get all active trips and respective postitions from all trucks
-router.get("/all", getAllTrips);
+router.get("/", getAllTrips);
 
 module.exports = router;
